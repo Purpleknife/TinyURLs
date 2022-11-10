1c0ea77c78dd6e40f510cd408f3e283f50b92440
Hind <hind.nayhi@gmail.com>
2022-11-10 14:50:17 -0700

    Add axios request to login users.

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -1,9 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+
+import { useCookies } from 'react-cookie';
+
+import { useNavigate } from 'react-router';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
 
+import axios from 'axios';
+import bcrypt from 'bcryptjs';
 
 interface LoginProps {
   handleClose: () => void;
@@ -11,9 +18,54 @@ interface LoginProps {
 }
 
 const Login = (props: LoginProps) => {
+  const [cookies, setCookie] = useCookies(['username', 'user_id', 'logged_in']);
+  const [showError, setShowError] = useState('');
 
-  const emailInput = useRef(null);
-  const passwordInput = useRef(null);
+  const emailInput = useRef<any>(null);
+  const passwordInput = useRef<any>(null);
+
+  const navigate = useNavigate();
+
+  interface User {
+    username: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  }
+
+  const loginUser = async() => {
+    const email = emailInput.current.value;
+    const password = passwordInput.current.value;
+
+    axios.get('/users')
+      .then(res => {
+        const allUsers = res.data;
+
+        const fetchUser = allUsers.find((user: User) => user.email === email);
+          if (fetchUser) {
+            if (bcrypt.compareSync(password, fetchUser.password)) {
+              const user_id = fetchUser.id;
+
+              axios.get(`/login/${user_id}`)
+                .then((res) => {
+                  console.log('login data', res.data);
+                  setCookie('username', res.data.username, {path: '/'});
+                  setCookie('user_id', res.data.id, {path: '/'});
+                  setCookie('logged_in', 'yes', {path: '/'});
+                  setShowError('');
+                  navigate(`/dashboard/${user_id}`);
+                })
+                .catch(error => console.log(error.message));
+            } else {
+              setShowError('Wrong credentials.');
+            }
+          }
+          if (!fetchUser) {
+            setShowError('This email adress is not registered.');
+          }
+      })
+      .catch(error => console.log(error.message));
+  };
 
   return (
     <>
@@ -21,6 +73,11 @@ const Login = (props: LoginProps) => {
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
+
+        {showError && <Alert id='alert' key='danger' variant='danger'>
+        {showError}
+        </Alert>}
+
         <Modal.Body>
           <Form>
 
@@ -50,7 +107,7 @@ const Login = (props: LoginProps) => {
         </Modal.Body>
         <Modal.Footer>
 
-          <Button id='login'>
+          <Button id='login' onClick={loginUser}>
             Login
           </Button>
         </Modal.Footer>
