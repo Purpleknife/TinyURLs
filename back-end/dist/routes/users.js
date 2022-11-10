0099dd7e356742e0c8a76190e33c7b4cbf0421eb
Hind <hind.nayhi@gmail.com>
2022-11-10 13:56:38 -0700

    Registering works properly now.

--- back-end/dist/routes/users.js
+++ back-end/dist/routes/users.js
@@ -8,6 +8,18 @@ dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 module.exports = (db) => {
+    // Route to fetch ALL users:
+    router.get('/users', (req, res) => {
+        const queryString = `SELECT * FROM users;`;
+        db.query(queryString)
+            .then((data) => {
+            console.log('res in route for USERS', data.rows);
+            res.json(data.rows);
+        })
+            .catch((error) => {
+            console.log(error.message);
+        });
+    });
     // Route to register new users:
     router.post('/users', (req, res) => {
         const username = req.body.username;
@@ -16,12 +28,17 @@ module.exports = (db) => {
         const password_confirmation = req.body.password_confirmation;
         const queryParams = [username, email, password, password_confirmation];
         const queryString = `
-      INSERT INTO users (username, first_name, last_name, email, password, password_confirmation)
-      VALUES ($1, $2, $3, $4, $5, $6)
+      INSERT INTO users (username, email, password, password_confirmation)
+      VALUES ($1, $2, $3, $4)
       RETURNING *;`;
-        db.query(queryString, queryParams);
-        console.log('res in route', res);
-        return res.send(res);
+        db.query(queryString, queryParams)
+            .then((data) => {
+            console.log('res in route for REGISTER', data.rows);
+            res.json(data.rows);
+        })
+            .catch((error) => {
+            console.log(error.message);
+        });
     });
     return router;
 };
--- front-end/src/components/Register.tsx
+++ front-end/src/components/Register.tsx
@@ -1,8 +1,15 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
+
+import { useCookies } from 'react-cookie';
+
+import axios from 'axios';
+import bcrypt from 'bcryptjs';
 
 
 interface RegisterProps {
@@ -11,10 +18,62 @@ interface RegisterProps {
 }
 
 const Register = (props: RegisterProps) => {
-  const usernameInput = useRef(null);
-  const emailInput = useRef(null);
-  const passwordInput = useRef(null);
-  const passwordConfirmationInput = useRef(null);
+  const [cookies, setCookie] = useCookies(['username', 'user_id', 'logged_in']);
+  const [showError, setShowError] = useState('');
+
+  const usernameInput = useRef<any>(null);
+  const emailInput = useRef<any>(null);
+  const passwordInput = useRef<any>(null);
+  const passwordConfirmationInput = useRef<any>(null);
+
+  const navigate = useNavigate();
+  const salt = bcrypt.genSaltSync(10);
+
+  interface User {
+    username: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+  }
+
+  const registerUser = async() => {
+    const password = passwordInput.current.value;
+    const saltedPassword = bcrypt.hashSync(password, salt);
+
+    const passwordConfirmation = passwordConfirmationInput.current.value;
+    const saltedPasswordConfirmation = bcrypt.hashSync(passwordConfirmation, salt);
+
+    axios.get('/users')
+      .then(res => {    
+        const allUsers = res.data;
+        const fetchUser = allUsers.find((user: User) => user.email === emailInput.current.value);
+        if (fetchUser) {
+          setShowError('This email adress is already registered.');
+        } else {
+          axios.post('/users', {
+            username: usernameInput!.current!.value,
+            email: emailInput.current.value,
+            password: saltedPassword, //=> BCRYPT
+            password_confirmation: saltedPasswordConfirmation
+          })
+            .then(res => {
+              console.log('res from front-end', res.data);
+              setCookie('username', res.data.username, {path: '/'});
+              setCookie('user_id', res.data.id, {path: '/'});
+              setCookie('logged_in', 'yes', {path: '/'});
+              setShowError('');
+              const user_id = res.data.id;
+              //navigate(`/dashboard/${user_id}`);
+            })
+            .catch((error) => {
+              console.log(error.message);
+            })
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
 
   return (
     <>
@@ -23,6 +82,11 @@ const Register = (props: RegisterProps) => {
         <Modal.Header closeButton>
           <Modal.Title>Register</Modal.Title>
         </Modal.Header>
+
+        {showError && <Alert id='alert' key='danger' variant='danger'>
+        {showError}
+        </Alert>}
+
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="registerForm.ControlInput1">
@@ -73,7 +137,7 @@ const Register = (props: RegisterProps) => {
         </Modal.Body>
         <Modal.Footer>
 
-          <Button id='register'>
+          <Button id='register' onClick={registerUser}>
             Register
           </Button>
         </Modal.Footer>
