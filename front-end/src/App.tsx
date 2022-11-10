9eefdfd3762bc6911cbb6a4a82d012cf8f1de511
Hind <hind.nayhi@gmail.com>
2022-11-10 14:34:59 -0700

    Add route for Dashboard.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-        </Routes>      
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/dashboard/:user_id' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
  
