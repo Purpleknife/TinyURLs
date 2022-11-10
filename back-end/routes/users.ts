0134808e40849d2476635241636be98957c5e461
Hind <hind.nayhi@gmail.com>
2022-11-10 13:28:07 -0700

    Implement routes for register + to fetch all users.

--- back-end/routes/users.ts
+++ back-end/routes/users.ts
@@ -5,6 +5,20 @@ import express, { Express, Request, Response } from 'express';
 const router  = express.Router();
 
 module.exports = (db: any) => {
+
+  // Route to fetch ALL users:
+  router.get('/users', (req: Request, res: Response) => {
+    const queryString: string = `SELECT * FROM users;`;
+
+    db.query(queryString)
+      .then((data: any) => {
+        console.log('res in route for USERS', data.rows);
+        res.json(data.rows);
+      })
+      .catch((error: Error) => {
+        console.log(error.message);
+      });
+  });
   
   // Route to register new users:
   router.post('/users', (req: Request, res: Response) => {
@@ -15,14 +29,19 @@ module.exports = (db: any) => {
 
     const queryParams: string[] = [username, email, password, password_confirmation];
     const queryString: string = `
-      INSERT INTO users (username, first_name, last_name, email, password, password_confirmation)
-      VALUES ($1, $2, $3, $4, $5, $6)
+      INSERT INTO users (username, email, password, password_confirmation)
+      VALUES ($1, $2, $3, $4)
       RETURNING *;`
     ;
     
-    db.query(queryString, queryParams)
-      console.log('res in route', res);
-      return res.send(res);
+    db.query(queryString, queryParams)      
+      .then((data: any) => {
+        console.log('res in route for REGISTER', data.rows);
+        res.json(data.rows);
+      })
+      .catch((error: Error) => {
+        console.log(error.message);
+      });
   })
 
   return router;
