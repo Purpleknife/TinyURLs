f1ecc6d1df4720a5df852357a0362f38f76ec0dd
Hind <hind.nayhi@gmail.com>
2022-11-14 12:12:48 -0700

    Implement a POST route to add a user's short URL to the db.

--- back-end/routes/users.ts
+++ back-end/routes/users.ts
@@ -62,28 +62,59 @@ module.exports = (db: any) => {
   });
 
 
-    // Route to get the user's Dashboard:
-    router.get('/dashboard/:user_id', (req: Request, res: Response) => {
-      const user_id: string | number = req.params.user_id;
 
-      const queryParams: (string | number)[] = [user_id];
-      const queryString: string = `SELECT * FROM urls WHERE user_id = $1;`
+  // Route for logout:
+  router.get('/logout', (req: Request, res: Response) => {
+    return res.json('You\'re logged out!');
+  });
+
+
+
+  // Route to get the user's Dashboard:
+  router.get('/dashboard/:user_id', (req: Request, res: Response) => {
+    const user_id: string | number = req.params.user_id;
+
+    const queryParams: (string | number)[] = [user_id];
+    const queryString: string = `SELECT * FROM urls WHERE user_id = $1;`
+
+    db.query(queryString, queryParams)
+      .then((data: any) => {
+        res.json(data.rows);
+      })
+      .catch((error: Error) => {
+        console.log(error.message);
+      });
+
+  });
+
+
+
+  //Route to add a user's short URL to the db:
+  router.post('/dashboard/:user_id', (req: Request, res: Response) => {
+    const user_id: string | number = req.params.user_id;
+    const title: string = req.body.title;
+    const long_url: string = req.body.long_url;
+    const short_url: string = req.body.short_url;
+
+    const queryParams: (string | number)[] = [user_id, long_url, short_url, title];
+    const queryString: string = `
+    INSERT INTO urls (user_id, date_created, long_url, short_url, title)
+    VALUES ($1, CURRENT_DATE, $2, $3, $4)
+    RETURNING *;
+    `;
+
+    db.query(queryString, queryParams)
+      .then((data: any) => {
+        res.json(data.rows);
+      })
+      .catch((error: Error) => {
+        console.log(error.message);
+      });
   
-      db.query(queryString, queryParams)
-        .then((data: any) => {
-          res.json(data.rows);
-        })
-        .catch((error: Error) => {
-          console.log(error.message);
-        });
-  
-    });
+  });
 
 
-    // Route for logout:
-    router.get('/logout', (req: Request, res: Response) => {
-      return res.json('You\'re logged out!');
-    });
+    
 
 
   return router;
