38205a12968a67d3cfc15f69485ba525f0cb6963
Hind <hind.nayhi@gmail.com>
2022-11-14 20:08:49 -0700

    Style Landing Page.

--- front-end/src/components/LandingPage.tsx
+++ front-end/src/components/LandingPage.tsx
@@ -19,16 +19,53 @@ const LandingPage = () => {
   const handleLoginShow = () => setShowLogin(true);
 
   return (
-    <div className="LandingPage">
-      <Button className='login' onClick={handleLoginShow}>
-        Login
-      </Button>
-      <Login handleClose={handleLoginClose} show={showLogin}/>
+    <div className="landing_page">
+
+      <div className='navbar_lp'>
+        <div className='logo'>
+          TinyURLs<i className="fa-solid fa-scissors"></i>
+        </div>
+
+        <div className='navbar_btn_lp'>
+            <Button className='login_register' onClick={handleLoginShow}>
+              Login
+            </Button>
+            <Login handleClose={handleLoginClose} show={showLogin}/>
+            &nbsp;&nbsp;
+            <Button className='login_register' onClick={handleRegisterShow}>
+              Register
+            </Button>
+            <Register handleClose={handleRegisterClose} show={showRegister}/>
+          </div>
+
+      </div>
       
-      <Button className='login' onClick={handleRegisterShow}>
-        Register
-      </Button>
-      <Register handleClose={handleRegisterClose} show={showRegister}/>
+      <div className='container_lp'>
+        <div className='catchphrase'>
+          
+          <span>
+            With TinyURLs, you can shorten your URLs and save them in your Dashboard.
+            <br />
+            Login or Register to get started.
+          </span>
+
+          <div className='getting_started'>
+            <Button className='login_register' onClick={handleRegisterShow}>
+              Getting Started
+            </Button>
+            <Register handleClose={handleRegisterClose} show={showRegister}/>
+          </div>
+        </div>
+
+        <div className='landing_img'>
+          <img
+            src='images/url.png'
+            alt='logo-img'
+          />
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
