a50010e21aad6a61e99f8cf8b217bba78a8c5cdf
Hind <hind.nayhi@gmail.com>
2022-11-17 13:35:22 -0700

    Style the form that shortens the URLs.

--- front-end/src/components/ShortenURLs.tsx
+++ front-end/src/components/ShortenURLs.tsx
@@ -19,12 +19,12 @@ const ShortenURLs = (props: ShortenURLsProps) => {
   const [cookies, setCookie] = useCookies(['username', 'user_id', 'logged_in']);
   const user_id = cookies.user_id;
 
-  const [shortURL, setShortURL] = useState<any>(null);
+  const [shortURL, setShortURL] = useState<any>('');
+  const [longURL, setLongURL] = useState<string>('');
+  const [saveLongURL, setSaveLongURL] = useState<string>('');
   const [urlCopied, setUrlCopied] = useState<boolean>(false);
   const [save, setSave] = useState<boolean>(false);
   
-
-  const longURLInput = useRef<any>(null);
   const title = useRef<any>(null);
 
   //Save to database:
@@ -44,13 +44,13 @@ const ShortenURLs = (props: ShortenURLsProps) => {
     const generatedShortURL: string = generateRandomShortURL();
 
     setShortURL(generatedShortURL);
+    setSaveLongURL(longURL);
+    setLongURL('');
   };
 
 
   //Redirect from short URL to long URL:
   const redirectToLongURL = () => {
-    const longURL = longURLInput.current.value;
-
     window.open(longURL);
   };
 
@@ -58,13 +58,15 @@ const ShortenURLs = (props: ShortenURLsProps) => {
   //Save generated URL to database:
   const saveToDatabase = () => {
     return axios.post(`/dashboard/${user_id}`, {
-      long_url: longURLInput.current.value,
+      long_url: longURL,
       short_url: shortURL,
       title: title.current.value
     })
       .then((res) => {
         console.log('Data saved', res.data);
         props.fetch();
+        setShortURL('');
+        title.current.value = '';
       })
       .catch((error) => {
         console.log(error.message);
@@ -79,42 +81,57 @@ const ShortenURLs = (props: ShortenURLsProps) => {
           placeholder='Enter your URL'
           type='longurl'
           name='longurl'
-          ref={longURLInput}
-        />&nbsp;
-      <button data-testid='shorten' type='submit' onClick={shortenURL}>Shorten</button>
-      </div>
-      {shortURL && 
-      <div>
-        <div data-testid='tiny-url' className='short_url' onClick={redirectToLongURL}>
-          {shortURL}
-        </div>
-
-        <OverlayTrigger
-          key='right'
-          placement='right'
-          overlay={
-            <Tooltip id='tooltip-right'>
-              {urlCopied ? 'Copied!' : 'Copy to clipboard.'}
-            </Tooltip>
+          value={longURL}
+          onChange = {(event) => {
+            setLongURL(event.target.value)}
           }
-        >
-          <i onClick={copyURL} data-testid='copy' className="fa-solid fa-copy"></i>
-        </OverlayTrigger>
+        />&nbsp;
+      <button className='shorten_btn' data-testid='shorten' type='submit' onClick={shortenURL}>Shorten</button>
+      </div>
+      {shortURL &&
+      <div className='tiny_url_div'>
+        <span data-testid='tiny-url' className='short_url' onClick={redirectToLongURL}>
+          {/* {shortURL} */}
+
+          <OverlayTrigger
+            key='bottom'
+            placement='bottom'
+            overlay={
+              <Tooltip id='tooltip-bottom'>
+                Click to open: {saveLongURL}.
+              </Tooltip>
+            }
+          >
+            <span>{shortURL}</span>
+          </OverlayTrigger>
+
+        </span>
+
+          <OverlayTrigger
+            key='right'
+            placement='right'
+            overlay={
+              <Tooltip id='tooltip-right'>
+                {urlCopied ? 'Copied!' : 'Copy to clipboard.'}
+              </Tooltip>
+            }
+          >
+            <i onClick={copyURL} data-testid='copy' className="fa-solid fa-copy"></i>
+          </OverlayTrigger>
       </div>
       }
+      <br />
+      <p>You want to save it? <i onClick={showSave} data-testid='save' className="fa-solid fa-chevron-down"></i></p>
 
-      <span>You want to save it? <i onClick={showSave} data-testid='save' className="fa-solid fa-chevron-down"></i></span>
-
-      {save && 
-      <div>
+      <div className='save_div'>
       <input
-        placeholder='Add a title...'
+        placeholder='Add a title'
         type='title'
         name='title'
         ref={title}
       />&nbsp;
       <button type='submit' data-testid='save-btn' onClick={saveToDatabase}>Save</button>
-      </div>}
+      </div>
     </div>
   );
 }
